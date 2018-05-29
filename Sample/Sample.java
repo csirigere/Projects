b257dce51c7fa97bcbd065d23896f30c4568f688
Chirag Sirigere <cjsirigere@gmail.com>
2018-05-29 14:49:04 -0500

    This is the first commit with code.

---
+++ Sample/Sample.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * This is a sample project that I did.
+ * 
+ * @author Chirag Sirigere
+ * @version 05/29/2018
+ */
+public class Sample
+{
+    private String task = "";
+    private int ID;
+    private Scanner s;
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void createTask()
+    {
+    }
+}
